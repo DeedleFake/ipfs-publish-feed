c00fb0397d32bc235e3545ca250d4630dbae2b44
DeedleFake <deedlefake@users.noreply.github.com>
2020-11-18 23:20:23 -0500

    internal/cli: godoc

--- internal/cli/signal.go
+++ internal/cli/signal.go
@@ -6,6 +6,8 @@ import (
 	"os/signal"
 )
 
+// SignalContext returns a child context that is canceled when any one
+// of the provided signals is received.
 func SignalContext(ctx context.Context, signals ...os.Signal) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 	go func() {
